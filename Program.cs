86e941bedd9177a4913bca4f3af854d0ea7bf58a
Макс <yashanty@gmail.com>
2021-09-10 15:46:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Combinatorics.Collections;
+
+namespace Permutation1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2017;
+            var arr = number.ToString().ToArray().Select(i=>int.Parse(i.ToString())).ToArray();
+            var perm = new Permutations<int>(arr);
+            var bigList = new List<int>();
+            foreach (var p in perm)
+            {
+                var smallList = new string[number.ToString().Length];
+                for (int i = 0; i < number.ToString().Length; i++)
+                {
+                    smallList[i]=p[i].ToString();
+                }
+                bigList.Add(int.Parse(string.Join("", smallList)));
+            }
+            bigList.OrderBy(w => w.ToString().Sum(c => char.GetNumericValue(c)));
+            var res = bigList[bigList.IndexOf(number)+1];
+
+        }
+    }
+}
